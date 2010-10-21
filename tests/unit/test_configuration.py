35288c619f9dcb4246304f7af738c5a78b4014fa
Alex Lopatin <alex@getaround.com>
2010-10-21 13:58:25 -0700

    Removing everything except for the core braintree library.

--- tests/unit/test_configuration.py
+++
@@ -1,5 +0,0 @@
-from tests.test_helper import *
-
-class TestConfiguration(unittest.TestCase):
-    def test_base_merchant_path_for_development(self):
-        self.assertTrue("/merchants/integration_merchnat_id", Configuration.instantiate().base_merchant_path())
--- tests/unit/test_http.py
+++
@@ -1,9 +0,0 @@
-from tests.test_helper import *
-
-class TestHttp(unittest.TestCase):
-    def test_raise_exception_from_status_for_upgrade_required(self):
-        try:
-            Http.raise_exception_from_status(426)
-            self.assertTrue(False)
-        except UpgradeRequiredError:
-            pass
